faa129ca8f6ac96e05400649c9ce52c0bf13a411
Anton Gorbachev <shieldmedia@yandex.ru>
2021-03-13 06:56:09 +0700

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  if (n < 0) {
+    n = -n
+  }
+  let numbersList = String(n).split("")
+  let reversedNumber = ""
+  for (i = numbersList.length - 1; i >= 0; i--) {
+    reversedNumber += numbersList[i];
+  }
+  return reversedNumber
 }
